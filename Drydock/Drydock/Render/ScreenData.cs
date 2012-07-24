20130e4c6d97ff64e693e0f0766ec64b956ea930
Ben Samuels <bsamuels453@gmail.com>
2012-07-24 13:52:57 -0700

    adding a screendata class for percentage-based measurements

---
+++ Drydock/Drydock/Render/ScreenData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drydock.Render {
+    static class ScreenData {
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+
+        static public void Init(int sizeX, int sizeY){
+            ScreenWidth = sizeX;
+            ScreenHeight = sizeY;
+        }
+
+        static public void GetScreenValue(float percentX, float percentY, ref int x, ref int y){
+            x = (int) (ScreenWidth * percentX);
+            y = (int)(ScreenHeight * percentY);
+        }
+
+    }
+}
