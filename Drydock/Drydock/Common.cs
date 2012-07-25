ae06b2e2c68e5c4319832795d4060d7972497399
Ben Samuels <bsamuels453@gmail.com>
2012-07-24 21:03:39 -0700

    adding a common class for commonly used functions

---
+++ Drydock/Drydock/Common.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Drydock{
+    internal static class Common{
+        public static Vector2 GetComponent(float angle, float length){ //LINEAR ALGEBRA STRIKES BACK
+            var up = new Vector2(1, 0);
+            Matrix rotMatrix = Matrix.CreateRotationZ(angle);
+            Vector2 direction = Vector2.Transform(up, rotMatrix);
+            return new Vector2(direction.X*length, direction.Y*length);
+        }
+    }
+}
