41aaea47456721f980dbc9301cd0e65b5afa1712
Ben Samuels <bsamuels453@gmail.com>
2012-07-24 21:17:51 -0700

    minor documentation update

--- Drydock/Drydock/Logic/IClickSubbable.cs
+++ Drydock/Drydock/Logic/IClickSubbable.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework.Input;
 
 namespace Drydock.Logic{
+    /// <summary>
+    /// allows a class to be subscribed for a mouse click event
+    /// </summary>
     internal interface IClickSubbable{
         /// <summary>
         /// Handle a fresh mouse click event. Return true if you want all other mouse click event hooks to be canceled for this update.
