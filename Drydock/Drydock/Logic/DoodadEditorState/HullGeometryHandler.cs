b22a351e415b8d64671f5d862cab21bbc08441df
Ben Samuels <bsamuels453@gmail.com>
2012-09-22 17:04:23 -0700

    implemented the generation of "deck geometry". aka floorboards.

--- Drydock/Drydock/Logic/DoodadEditorState/HullGeometryHandler.cs
+++ Drydock/Drydock/Logic/DoodadEditorState/HullGeometryHandler.cs
@@ -1,5 +1,6 @@
 ﻿#region
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Drydock.Control;
@@ -69,31 +70,70 @@ namespace Drydock.Logic.DoodadEditorState{
             //generate a normals array for the entire ship, rather than per-deck
             var totalMesh = new Vector3[_layerVerts.Count,_layerVerts[0].Count];
             var totalNormals = new Vector3[_layerVerts.Count,_layerVerts[0].Count];
-            MeshHelper.Encode2DListIntoMesh(_layerVerts.Count, _layerVerts[0].Count, ref totalMesh, _layerVerts);
+            MeshHelper.Encode2DListIntoArray(_layerVerts.Count, _layerVerts[0].Count, ref totalMesh, _layerVerts);
             MeshHelper.GenerateMeshNormals(totalMesh, ref totalNormals);
 
+            //now generate the floor for each deck
+            var deckMesh = new List<Vector3[,]>();
+            var deckNormals = new Vector3[3, _layerVerts[0].Count / 2];
+            for (int x = 0; x < 3; x++){
+                for (int z = 0; z < _layerVerts[0].Count / 2; z++){
+                    deckNormals[x, z] = Vector3.Up;
+                }
+            }
+            for (int deck = 0; deck < numDecks; deck ++) {
+                deckMesh.Add(new Vector3[3, _layerVerts[0].Count / 2]);
+                for (int vert = 0; vert < _layerVerts[0].Count / 2; vert++){
+                    deckMesh.Last()[0, vert] = _layerVerts[deck *deckPrimitiveHeight][_layerVerts[0].Count / 2 + vert];
+
+                    deckMesh.Last()[1, vert] = _layerVerts[deck * deckPrimitiveHeight][_layerVerts[0].Count / 2 + vert];
+                    deckMesh.Last()[2, vert] = _layerVerts[deck * deckPrimitiveHeight][_layerVerts[0].Count / 2 - vert - 1];
+                    deckMesh.Last()[1, vert].Z = 0;
+
+                    deckMesh.Last()[2, vert] = _layerVerts[deck * deckPrimitiveHeight][_layerVerts[0].Count / 2 - vert -1];
+                }
+            }
 
             //now set up the display buffer for each deck
             for (int i = 0; i < _deckVertexes.Count; i++){
                 var mesh = new Vector3[deckPrimitiveHeight + 1,_deckVertexes[0][0].Count];
                 var normals = new Vector3[deckPrimitiveHeight + 1,_deckVertexes[0][0].Count];
-                int[] indicies = MeshHelper.CreateIndiceArray((deckPrimitiveHeight + 1)*_deckVertexes[0][0].Count);
-                VertexPositionNormalTexture[] verticies = MeshHelper.CreateTexcoordedVertexList((deckPrimitiveHeight + 1)*_deckVertexes[0][0].Count);
-
+                int[] hullIndicies = MeshHelper.CreateIndiceArray((deckPrimitiveHeight + 1)*_deckVertexes[0][0].Count);
+                VertexPositionNormalTexture[] hullVerticies = MeshHelper.CreateTexcoordedVertexList((deckPrimitiveHeight + 1)*_deckVertexes[0][0].Count);
 
                 for (int x = 0; x < deckPrimitiveHeight + 1; x++){
                     for (int z = 0; z < _deckVertexes[0][0].Count; z++){
                         normals[x, z] = totalNormals[i*deckPrimitiveHeight + x, z];
                     }
                 }
+                MeshHelper.Encode2DListIntoArray(deckPrimitiveHeight + 1, _deckVertexes[0][0].Count, ref mesh, _deckVertexes[i]);
+                MeshHelper.ConvertMeshToVertList(mesh, normals, ref hullVerticies);
 
-                MeshHelper.Encode2DListIntoMesh(deckPrimitiveHeight + 1, _deckVertexes[0][0].Count, ref mesh, _deckVertexes[i]);
-                //MeshHelper.GenerateMeshNormals(mesh, ref normals);
-                MeshHelper.ConvertMeshToVertList(mesh, normals, ref verticies);
+                //now generate the deck geometry<
+                var deckVerticies = new VertexPositionNormalTexture[0];
+                var deckIndicies = new int[0];
+                if (i != _deckVertexes.Count - 1) {//exception based on how the very very bottom section does not have a designated "deck"
+                    deckVerticies = MeshHelper.CreateTexcoordedVertexList(4 * _layerVerts[0].Count / 2);
+                    deckIndicies = MeshHelper.CreateIndiceArray(4 * _layerVerts[0].Count / 2);
 
-                _deckBuffers[i] = new ShipGeometryBuffer(indicies.Count(), verticies.Count(), verticies.Count()/2, "whiteborder");
-                _deckBuffers[i].Indexbuffer.SetData(indicies);
-                _deckBuffers[i].Vertexbuffer.SetData(verticies);
+
+                    for (int si = 0; si < deckIndicies.Count(); si++) { //offset the deck indicies to be after the hull indicies
+                        deckIndicies[si] += hullVerticies.Length;
+                    }
+                    MeshHelper.ConvertMeshToVertList(deckMesh[i], deckNormals, ref deckVerticies);
+                }
+
+                var totalVerticies = new VertexPositionNormalTexture[hullVerticies.Length + deckVerticies.Length];
+                hullVerticies.CopyTo(totalVerticies, 0);
+                deckVerticies.CopyTo(totalVerticies, hullVerticies.Length);
+
+                var totalIndicies = new int[hullIndicies.Length + deckIndicies.Length];
+                hullIndicies.CopyTo(totalIndicies, 0);
+                deckIndicies.CopyTo(totalIndicies, hullIndicies.Length);
+
+                _deckBuffers[i] = new ShipGeometryBuffer(totalIndicies.Length, totalVerticies.Length, totalIndicies.Length/3, "whiteborder");
+                _deckBuffers[i].Indexbuffer.SetData(totalIndicies);
+                _deckBuffers[i].Vertexbuffer.SetData(totalVerticies);
             }
 
             //get center point
