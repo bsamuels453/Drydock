356ee4c5181cad75d8e3533594a885ba15bae952
Ben Samuels <bsamuels453@gmail.com>
2012-09-28 16:19:09 -0700

    fixed a bug where all effects were referencing the same effect

--- Drydock/Drydock/Render/ShipGeometryBuffer.cs
+++ Drydock/Drydock/Render/ShipGeometryBuffer.cs
@@ -18,7 +18,7 @@ namespace Drydock.Render{
             _rasterizerState.CullMode = cullMode;
 
             _texture = Singleton.ContentManager.Load<Texture2D>(textureName);
-            _effect = Singleton.ContentManager.Load<Effect>("StandardEffect");
+            _effect = Singleton.ContentManager.Load<Effect>("StandardEffect").Clone();
             _effect.Parameters["Projection"].SetValue(Singleton.ProjectionMatrix);
             _effect.Parameters["World"].SetValue(Matrix.Identity);
             _effect.Parameters["AmbientIntensity"].SetValue(1.25f);
@@ -38,8 +38,15 @@ namespace Drydock.Render{
             set { _rasterizerState.CullMode = value; }
         }
 
+        public Effect Effect { get { return _effect; }
+        }
+
         public Vector3 DiffuseDirection{
             set { _effect.Parameters["DiffuseLightDirection"].SetValue(value);  }
+        }
+
+        public float AmbientIntensity {
+            set { _effect.Parameters["AmbientIntensity"].SetValue(value); }
         }
 
         protected override void UpdateEffectParams(Matrix viewMatrix){
