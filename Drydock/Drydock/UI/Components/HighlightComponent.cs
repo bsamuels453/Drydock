5fcfe985683d33164c84b4efc0e0dd54f46afed4
Ben Samuels <bsamuels453@gmail.com>
2012-10-20 12:27:28 -0700

    updating highlightcomponent with a new setting allowing the highlight mask to trigger when the button is clicked

--- Drydock/Drydock/UI/Components/HighlightComponent.cs
+++ Drydock/Drydock/UI/Components/HighlightComponent.cs
@@ -8,19 +8,54 @@ using Newtonsoft.Json.Linq;
 #endregion
 
 namespace Drydock.UI.Components{
-    internal class HighlightComponent : IUIComponent, IAcceptMouseMovementEvent{
+    internal class HighlightComponent : IUIComponent, IAcceptMouseMovementEvent, IAcceptLeftButtonPressEvent, IAcceptLeftButtonReleaseEvent{
+        #region HighlightTrigger enum
+
+        public enum HighlightTrigger{
+            InvalidTrigger,
+            MouseEntryExit,
+            MousePressRelease
+        }
+
+        #endregion
+
         readonly string _highlightTexture;
+        readonly HighlightTrigger _highlightTrigger;
         Sprite2D _highlightSprite;
+        float _highlightTexOpacity;
         IUIInteractiveElement _owner;
 
-        public HighlightComponent(string highlightTexture){
+        public HighlightComponent(string highlightTexture, HighlightTrigger highlightTrigger, float highlightTexOpacity = 0.3f){
             _highlightTexture = highlightTexture;
+            _highlightTrigger = highlightTrigger;
+            _highlightTexOpacity = highlightTexOpacity;
         }
+
+        #region IAcceptLeftButtonPressEvent Members
+
+        public void OnLeftButtonPress(ref bool allowInterpretation, Point mousePos, Point prevMousePos){
+            if (_owner.ContainsMouse){
+                _highlightSprite.Opacity = _highlightTexOpacity;
+            }
+            else{
+                _highlightSprite.Opacity = 0;
+            }
+        }
+
+        #endregion
+
+        #region IAcceptLeftButtonReleaseEvent Members
+
+        public void OnLeftButtonRelease(ref bool allowInterpretation, Point mousePos, Point prevMousePos){
+            _highlightSprite.Opacity = 0;
+        }
+
+        #endregion
 
         #region IAcceptMouseMovementEvent Members
 
         public void OnMouseMovement(ref bool allowInterpretation, Point mousePos, Point prevMousePos){
-            _highlightSprite.Opacity = _owner.ContainsMouse ? 0.3f : 0;
+            _highlightSprite.Opacity = _owner.ContainsMouse ? _highlightTexOpacity : 0;
         }
 
         #endregion
@@ -33,14 +68,24 @@ namespace Drydock.UI.Components{
             _owner = (IUIInteractiveElement) owner;
 
             //event stuff
-            ownerEventDispatcher.OnMouseMovement.Add(this);
             if (owner.DoesComponentExist<DraggableComponent>()){
                 var dcomponent = _owner.GetComponent<DraggableComponent>();
                 dcomponent.DragMovementDispatcher += OnOwnerDrag;
             }
+            switch (_highlightTrigger){
+                case HighlightTrigger.MouseEntryExit:
+                    ownerEventDispatcher.OnMouseMovement.Add(this);
+                    break;
+                case HighlightTrigger.MousePressRelease:
+                    ownerEventDispatcher.OnLeftButtonPress.Add(this);
+                    ownerEventDispatcher.OnLeftButtonRelease.Add(this);
+                    break;
+                case HighlightTrigger.InvalidTrigger:
+                    throw new Exception("invalid highlight trigger");
+            }
 
             //create sprite
-            _highlightSprite = new Sprite2D(_highlightTexture, (int) _owner.X, (int) _owner.Y, (int) _owner.Width, (int) _owner.Height, owner.Depth - 0.01f, 0);
+            _highlightSprite = new Sprite2D(_highlightTexture, (int)_owner.X, (int)_owner.Y, (int)_owner.Width, (int)_owner.Height, _owner.Depth - 0.01f, 0);
         }
 
         public void Update(){
@@ -57,18 +102,20 @@ namespace Drydock.UI.Components{
         public static HighlightComponent ConstructFromObject(JObject obj){
             var data = obj.ToObject<HighlightComponentCtorData>();
 
-            if (data.HighlightTexture == null)
+            if (data.HighlightTexture == null || data.HighlightTrigger == HighlightTrigger.InvalidTrigger)
                 throw new Exception("not enough information to generate highlight component");
 
-            return new HighlightComponent(data.HighlightTexture);
+            return new HighlightComponent(data.HighlightTexture, data.HighlightTrigger);
         }
 
         #region Nested type: HighlightComponentCtorData
 
         struct HighlightComponentCtorData{
-// ReSharper disable UnassignedField.Local
+            // ReSharper disable UnassignedField.Local
             public string HighlightTexture;
-// ReSharper restore UnassignedField.Local
+            public HighlightTrigger HighlightTrigger;
+            public float HighlightTexOpacity;
+            // ReSharper restore UnassignedField.Local
         }
 
         #endregion
