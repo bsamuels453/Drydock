a50d658edc617e04e1abdaca5555878378ae9412
Ben Samuels <bsamuels453@gmail.com>
2012-10-20 21:53:38 -0700

    adding wiremesh for where walls can be placed during wall build mode

--- Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
+++ Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
@@ -18,28 +18,88 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
         readonly Vector3[][] _deckFloorVertexes;
         readonly WireframeBuffer _selectionBuff;
         readonly IntRefLambda _curDeck;
-        readonly ShipGeometryBuffer _guideDotBuffer;
+        readonly WireframeBuffer[] _guideDotBuffer;
+        bool _isEnabled;
 
         public WallBuildTool(HullGeometryInfo hullInfo, IntRef visibleDecks){
-            _deckFloorBoundingboxes = hullInfo.DeckFloorBoundingBoxes;
+            _isEnabled = false;
+            _deckFloorBoundingboxes = hullInfo.LowResFloorBoundingBoxes;
             _deckFloorVertexes = hullInfo.DeckFloorVertexes;
             _curDeck = new IntRefLambda(visibleDecks, input => hullInfo.NumDecks - input);
+            visibleDecks.RefModCallback += VisibleDeckChange;
 
             _selectionBuff = new WireframeBuffer(2, 2, 1);
             var selectionIndicies = new[]{0, 1};
             _selectionBuff.Indexbuffer.SetData(selectionIndicies);
             _selectionBuff.IsEnabled = false;
 
-            int numDots = _deckFloorVertexes[_curDeck.Value].Count();
-            _guideDotBuffer = new ShipGeometryBuffer(4 * numDots, 4 * numDots, numDots, "blackdot");
-            var guideDotIndicies = new int[4 * numDots];
-            for (int i = 0; i < 4 * numDots; i++)
-                guideDotIndicies[i] = i;
-            _guideDotBuffer.Indexbuffer.SetData(guideDotIndicies);
-            _guideDotBuffer.IsEnabled = true; 
+            _guideDotBuffer = new WireframeBuffer[hullInfo.NumDecks+1];
+
+            for (int i = 0; i < hullInfo.NumDecks+1; i++) {
+                #region indicies
+                int numBoxes = _deckFloorBoundingboxes[i].Count();
+                _guideDotBuffer[i] = new WireframeBuffer(8*numBoxes, 8*numBoxes, 4*numBoxes);
+                var guideDotIndicies = new int[8*numBoxes];
+                for (int si = 0; si < 8*numBoxes; si += 1){
+                    guideDotIndicies[si] = si;
+                }
+                _guideDotBuffer[i].Indexbuffer.SetData(guideDotIndicies);
+                
+                #endregion
+
+                #region verticies
+
+                var verts = new VertexPositionColor[_deckFloorBoundingboxes[i].Count() * 8];
+
+                int vertIndex = 0;
+
+                foreach (var boundingBox in _deckFloorBoundingboxes[i]) {
+                    Vector3 v1, v2, v3, v4;
+                    //v4  v3
+                    //
+                    //v1  v2
+                    v1 = boundingBox.Min;
+                    v2 = new Vector3(boundingBox.Min.X, boundingBox.Min.Y, boundingBox.Max.Z);
+                    v3 = boundingBox.Max;
+                    v4 = new Vector3(boundingBox.Max.X, boundingBox.Min.Y, boundingBox.Min.Z);
+
+                    v1.Y += 0.03f;
+                    v2.Y += 0.03f;
+                    v3.Y += 0.03f;
+                    v4.Y += 0.03f;
+
+
+                    verts[vertIndex] = new VertexPositionColor(v1, Color.Gray);
+                    verts[vertIndex + 1] = new VertexPositionColor(v2, Color.Gray);
+                    verts[vertIndex + 2] = new VertexPositionColor(v2, Color.Gray);
+                    verts[vertIndex + 3] = new VertexPositionColor(v3, Color.Gray);
+
+                    verts[vertIndex + 4] = new VertexPositionColor(v3, Color.Gray);
+                    verts[vertIndex + 5] = new VertexPositionColor(v4, Color.Gray);
+                    verts[vertIndex + 6] = new VertexPositionColor(v4, Color.Gray);
+                    verts[vertIndex + 7] = new VertexPositionColor(v1, Color.Gray);
+
+                    vertIndex += 8;
+                }
+                _guideDotBuffer[i].Vertexbuffer.SetData(verts);
+                #endregion
+
+                _guideDotBuffer[i].IsEnabled = false;
+            }
+
         }
 
         #region IToolbarTool Members
+
+        void VisibleDeckChange(IntRef caller, int oldVal, int newVal){
+            if (_isEnabled){
+                foreach (var buffer in _guideDotBuffer){
+                    buffer.IsEnabled = false;
+                }
+
+                _guideDotBuffer[_curDeck.Value].IsEnabled = true;
+            }
+        }
 
         public void UpdateInput(ref ControlState state) {
             var nearMouse = new Vector3(state.MousePos.X, state.MousePos.Y, 0);
@@ -97,9 +157,16 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
         }
 
         public void Enable(){
+            _isEnabled = true;
+            _selectionBuff.IsEnabled = true;
+            _guideDotBuffer[_curDeck.Value].IsEnabled = true;
         }
 
         public void Disable(){
+            foreach (var buffer in _guideDotBuffer){
+                buffer.IsEnabled = false;
+            }
+            _isEnabled = false;
             _selectionBuff.IsEnabled = false;
         }
 
