7984ade38721ea09d97466762caae9788d6ef88a
Ben Samuels <bsamuels453@gmail.com>
2012-10-20 19:49:16 -0700

    implementing guide dot buffer

--- Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
+++ Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
@@ -16,25 +16,32 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
     internal class WallBuildTool : IToolbarTool{
         readonly BoundingBox[][] _deckFloorBoundingboxes;
         readonly Vector3[][] _deckFloorVertexes;
-        readonly int _numDecks;
         readonly WireframeBuffer _selectionBuff;
-        readonly IntRef _visibleDecks;
+        readonly IntRefLambda _curDeck;
+        readonly ShipGeometryBuffer _guideDotBuffer;
 
         public WallBuildTool(HullGeometryInfo hullInfo, IntRef visibleDecks){
-            _visibleDecks = visibleDecks;
-            _numDecks = hullInfo.NumDecks;
             _deckFloorBoundingboxes = hullInfo.DeckFloorBoundingBoxes;
             _deckFloorVertexes = hullInfo.DeckFloorVertexes;
-            _selectionBuff = new WireframeBuffer(2, 2, 1);
+            _curDeck = new IntRefLambda(visibleDecks, input => hullInfo.NumDecks - input);
 
-            var indicies = new[]{0, 1};
-            _selectionBuff.Indexbuffer.SetData(indicies);
+            _selectionBuff = new WireframeBuffer(2, 2, 1);
+            var selectionIndicies = new[]{0, 1};
+            _selectionBuff.Indexbuffer.SetData(selectionIndicies);
             _selectionBuff.IsEnabled = false;
+
+            int numDots = _deckFloorVertexes[_curDeck.Value].Count();
+            _guideDotBuffer = new ShipGeometryBuffer(4 * numDots, 4 * numDots, numDots, "blackdot");
+            var guideDotIndicies = new int[4 * numDots];
+            for (int i = 0; i < 4 * numDots; i++)
+                guideDotIndicies[i] = i;
+            _guideDotBuffer.Indexbuffer.SetData(guideDotIndicies);
+            _guideDotBuffer.IsEnabled = true; 
         }
 
         #region IToolbarTool Members
 
-        public void UpdateInput(ref ControlState state){
+        public void UpdateInput(ref ControlState state) {
             var nearMouse = new Vector3(state.MousePos.X, state.MousePos.Y, 0);
             var farMouse = new Vector3(state.MousePos.X, state.MousePos.Y, 1);
 
@@ -57,28 +64,26 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
             direction.Normalize();
             var ray = new Ray(nearPoint, direction);
 
-
-            int deckIndex = _numDecks - _visibleDecks.Value;
             float? ndist;
-            for (int i = 0; i < _deckFloorBoundingboxes[deckIndex].Length; i++){
-                if ((ndist = ray.Intersects(_deckFloorBoundingboxes[deckIndex][i])) != null){
+            for (int i = 0; i < _deckFloorBoundingboxes[_curDeck.Value].Length; i++) {
+                if ((ndist = ray.Intersects(_deckFloorBoundingboxes[_curDeck.Value][i])) != null) {
                     var rayTermination = ray.Position + ray.Direction*(float) ndist;
 
                     var distList = new List<float>();
 
-                    for (int point = 0; point < _deckFloorVertexes[deckIndex].Count(); point++){
-                        distList.Add(Vector3.Distance(rayTermination, _deckFloorVertexes[deckIndex][point]));
+                    for (int point = 0; point < _deckFloorVertexes[_curDeck.Value].Count(); point++) {
+                        distList.Add(Vector3.Distance(rayTermination, _deckFloorVertexes[_curDeck.Value][point]));
                     }
                     float f = distList.Min();
                     int ptIdx = distList.IndexOf(f);
 
                     var verts = new VertexPositionColor[2];
-                    verts[0] = new VertexPositionColor(_deckFloorVertexes[deckIndex][ptIdx], Color.White);
+                    verts[0] = new VertexPositionColor(_deckFloorVertexes[_curDeck.Value][ptIdx], Color.White);
                     verts[1] = new VertexPositionColor(
                         new Vector3(
-                            _deckFloorVertexes[deckIndex][ptIdx].X,
-                            _deckFloorVertexes[deckIndex][ptIdx].Y + 10,
-                            _deckFloorVertexes[deckIndex][ptIdx].Z
+                            _deckFloorVertexes[_curDeck.Value][ptIdx].X,
+                            _deckFloorVertexes[_curDeck.Value][ptIdx].Y + 10,
+                            _deckFloorVertexes[_curDeck.Value][ptIdx].Z
                             ),
                         Color.White
                         );
