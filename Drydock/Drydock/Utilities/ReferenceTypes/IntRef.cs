0dbf182de0ce6648f85300122d62d650193e9260
Ben Samuels <bsamuels453@gmail.com>
2012-10-20 19:35:50 -0700

    adding modification callback to intref

--- Drydock/Drydock/Utilities/ReferenceTypes/IntRef.cs
+++ Drydock/Drydock/Utilities/ReferenceTypes/IntRef.cs
@@ -1,5 +1,17 @@
 ﻿namespace Drydock.Utilities.ReferenceTypes{
+    delegate void RefModCallback(IntRef caller, int oldValue, int newValue);
     internal class IntRef{
-        public int Value;
+        int _value;
+
+        public event RefModCallback RefModCallback;
+
+        public int Value{
+            get { return _value; }
+            set {
+                int oldValue = _value;
+                _value = value;
+                RefModCallback.Invoke(this, oldValue, _value);
+            }
+        }
     }
 }
