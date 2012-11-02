d4fa7bfbb81b52702ad708b415c92814e1cf9ff2
Ben Samuels <bsamuels453@gmail.com>
2012-11-01 17:51:26 -0700

    code cleanup

--- Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
+++ Drydock/Drydock/Logic/DoodadEditorState/Tools/WallBuildTool.cs
@@ -11,51 +11,64 @@ using Drydock.Utilities.ReferenceTypes;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Drydock.Render;
 
 #endregion
 
 namespace Drydock.Logic.DoodadEditorState.Tools{
     internal class WallBuildTool : IToolbarTool{
-        readonly float _wallResolution;
         readonly IntRefLambda _curDeck;
+        readonly ObjectBuffer[] _wallBuffers;
+        readonly List<WallIdentifier>[] _wallIdentifiers;
+        readonly float _wallResolution;
+
+        #region fields for maintaining the wall editor environment
+
         readonly BoundingBox[][] _deckFloorBoundingboxes;
         readonly Vector3[][] _deckFloorVertexes;
         readonly WireframeBuffer[] _guideGridBuffers;
-        readonly WireframeBuffer _selectionBuff;
-        readonly ObjectBuffer[] _wallBuffers;
-        readonly List<WallIdentifier>[] _wallIdentifiers;
+
+        #endregion
+
+        #region fields for the cursor and temp walls constructed by it
+
+        readonly WireframeBuffer _cursorBuff;
         readonly ObjectBuffer _tempWallBuffer;
         readonly List<WallIdentifier> _tempWallIdentifiers;
 
-        bool _isEnabled;
+        #endregion
+
+        #region tool state fields
+
         bool _cursorActive;
-        bool _isDrawing;
         Vector3 _cursorPosition;
-        Vector3 _strokeOrigin;
+        bool _isDrawing;
+        bool _isEnabled;
         Vector3 _strokeEnd;
-        
+        Vector3 _strokeOrigin;
 
-        ScreenText _strokeW;
-        ScreenText _strokeH;
+        #endregion
 
-        public WallBuildTool(HullGeometryInfo hullInfo, IntRef visibleDecksRef, ObjectBuffer[] wallBuffers, List<WallIdentifier>[] wallIdentifiers ){
+        public WallBuildTool(HullGeometryInfo hullInfo, IntRef visibleDecksRef, ObjectBuffer[] wallBuffers, List<WallIdentifier>[] wallIdentifiers){
+            #region set fields
+
             _isEnabled = false;
             _deckFloorBoundingboxes = hullInfo.DeckFloorBoundingBoxes;
             _deckFloorVertexes = hullInfo.FloorVertexes;
             _wallBuffers = wallBuffers;
             _wallIdentifiers = wallIdentifiers;
             _wallResolution = hullInfo.WallResolution;
-            _tempWallBuffer = new ObjectBuffer(hullInfo.FloorVertexes[0].Count()*2, 10, 20, 30, "brown"){UpdateBufferManually = true};
             _tempWallIdentifiers = new List<WallIdentifier>();
-
             _curDeck = new IntRefLambda(visibleDecksRef, input => hullInfo.NumDecks - input);
 
-            visibleDecksRef.RefModCallback += VisibleDeckChange;
-            _selectionBuff = new WireframeBuffer(2, 2, 1);
+            #endregion
+
+            #region set buffer stuff
+
+            _tempWallBuffer = new ObjectBuffer(hullInfo.FloorVertexes[0].Count()*2, 10, 20, 30, "brown"){UpdateBufferManually = true};
+            _cursorBuff = new WireframeBuffer(2, 2, 1);
             var selectionIndicies = new[]{0, 1};
-            _selectionBuff.Indexbuffer.SetData(selectionIndicies);
-            _selectionBuff.IsEnabled = false;
+            _cursorBuff.Indexbuffer.SetData(selectionIndicies);
+            _cursorBuff.IsEnabled = false;
 
             _guideGridBuffers = new WireframeBuffer[hullInfo.NumDecks + 1];
 
@@ -113,16 +126,16 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
                 _guideGridBuffers[i].IsEnabled = false;
             }
 
-            _strokeW = new ScreenText(0, 0, "w:");
-            _strokeH = new ScreenText(0, 20, "h:");
+            #endregion
+
+            visibleDecksRef.RefModCallback += VisibleDeckChange;
         }
 
         #region IToolbarTool Members
 
         public void UpdateInput(ref ControlState state){
-            
-
             var prevCursorPosition = _cursorPosition;
+
             #region update cursor
 
             var nearMouse = new Vector3(state.MousePos.X, state.MousePos.Y, 0);
@@ -147,7 +160,7 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
             direction.Normalize();
             var ray = new Ray(nearPoint, direction);
 
-            
+
             float? ndist;
             bool intersectionFound = false;
             for (int i = 0; i < _deckFloorBoundingboxes[_curDeck.Value].Length; i++){
@@ -181,32 +194,42 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
                             ),
                         Color.White
                         );
-                    _selectionBuff.Vertexbuffer.SetData(verts);
-                    _selectionBuff.IsEnabled = true;
+                    _cursorBuff.Vertexbuffer.SetData(verts);
+                    _cursorBuff.IsEnabled = true;
                     intersectionFound = true;
                     break;
                 }
             }
             if (!intersectionFound){
-                _selectionBuff.IsEnabled = false;
+                _cursorBuff.IsEnabled = false;
                 _cursorActive = false;
             }
 
             #endregion
+
+            #region handle mousedown
 
             if (
                 state.LeftButtonState != state.PrevState.LeftButtonState &&
                 state.LeftButtonState == ButtonState.Pressed
                 && _cursorActive
                 ){
-                    _strokeOrigin = _cursorPosition;
-                    _isDrawing = true;
+                _strokeOrigin = _cursorPosition;
+                _isDrawing = true;
             }
 
-            if (prevCursorPosition != _cursorPosition && _cursorActive && _isDrawing) {
+            #endregion
+
+            #region handle cursor movement
+
+            if (prevCursorPosition != _cursorPosition && _cursorActive && _isDrawing){
                 _strokeEnd = _cursorPosition;
                 GenerateWallsFromStroke();
             }
+
+            #endregion
+
+            #region handle cursor up
 
             if (_cursorActive && _isDrawing && state.LeftButtonState == ButtonState.Released){
                 _isDrawing = false;
@@ -217,13 +240,15 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
                 _wallBuffers[_curDeck.Value].AbsorbBuffer(_tempWallBuffer);
             }
 
+            #endregion
         }
 
-        public void UpdateLogic(double timeDelta){}
+        public void UpdateLogic(double timeDelta){
+        }
 
         public void Enable(){
             _isEnabled = true;
-            _selectionBuff.IsEnabled = true;
+            _cursorBuff.IsEnabled = true;
             _guideGridBuffers[_curDeck.Value].IsEnabled = true;
         }
 
@@ -232,7 +257,7 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
                 buffer.IsEnabled = false;
             }
             _isEnabled = false;
-            _selectionBuff.IsEnabled = false;
+            _cursorBuff.IsEnabled = false;
         }
 
         #endregion
@@ -249,10 +274,8 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
 
         void GenerateWallsFromStroke(){
             _tempWallIdentifiers.Clear();
-            int strokeW = (int) ((_strokeEnd.Z - _strokeOrigin.Z) / _wallResolution);
-            int strokeH = (int)((_strokeEnd.X - _strokeOrigin.X) / _wallResolution);
-            _strokeW.EditText("W:"+strokeW.ToString());
-            _strokeH.EditText("H"+strokeH.ToString());
+            int strokeW = (int) ((_strokeEnd.Z - _strokeOrigin.Z)/_wallResolution);
+            int strokeH = (int) ((_strokeEnd.X - _strokeOrigin.X)/_wallResolution);
 
             _tempWallBuffer.ClearObjects();
             int wDir;
@@ -267,40 +290,40 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
                 hDir = -1;
 
             //generate width walls
-            for (int i = 0; i < Math.Abs(strokeW); i++) {
+            for (int i = 0; i < Math.Abs(strokeW); i++){
                 int[] indicies;
                 VertexPositionNormalTexture[] verticies;
-                var origin = new Vector3(_strokeOrigin.X, _strokeOrigin.Y, _strokeOrigin.Z + _wallResolution * i * wDir);
-                MeshHelper.GenerateCube(out verticies, out indicies, origin, 0.1f, 1, _wallResolution * wDir);
-                var identifier = new WallIdentifier(origin, new Vector3(origin.X, origin.Y, origin.Z + _wallResolution * wDir));
+                var origin = new Vector3(_strokeOrigin.X, _strokeOrigin.Y, _strokeOrigin.Z + _wallResolution*i*wDir);
+                MeshHelper.GenerateCube(out verticies, out indicies, origin, 0.1f, 1, _wallResolution*wDir);
+                var identifier = new WallIdentifier(origin, new Vector3(origin.X, origin.Y, origin.Z + _wallResolution*wDir));
                 _tempWallBuffer.AddObject(identifier, indicies, verticies);
                 _tempWallIdentifiers.Add(identifier);
             }
-            for (int i = 0; i < Math.Abs(strokeW); i++) {
+            for (int i = 0; i < Math.Abs(strokeW); i++){
                 int[] indicies;
                 VertexPositionNormalTexture[] verticies;
-                var origin = new Vector3(_strokeEnd.X, _strokeOrigin.Y, _strokeOrigin.Z + _wallResolution * i * wDir);
-                MeshHelper.GenerateCube(out verticies, out indicies, origin, 0.1f, 1, _wallResolution * wDir);
-                var identifier = new WallIdentifier(origin, new Vector3(origin.X, origin.Y, origin.Z + _wallResolution * wDir));
+                var origin = new Vector3(_strokeEnd.X, _strokeOrigin.Y, _strokeOrigin.Z + _wallResolution*i*wDir);
+                MeshHelper.GenerateCube(out verticies, out indicies, origin, 0.1f, 1, _wallResolution*wDir);
+                var identifier = new WallIdentifier(origin, new Vector3(origin.X, origin.Y, origin.Z + _wallResolution*wDir));
                 _tempWallBuffer.AddObject(identifier, indicies, verticies);
                 _tempWallIdentifiers.Add(identifier);
             }
             //generate height walls
-            for (int i = 0; i < Math.Abs(strokeH); i++) {
+            for (int i = 0; i < Math.Abs(strokeH); i++){
                 int[] indicies;
                 VertexPositionNormalTexture[] verticies;
-                var origin = new Vector3(_strokeOrigin.X + _wallResolution * i * hDir, _strokeOrigin.Y, _strokeOrigin.Z);
-                MeshHelper.GenerateCube(out verticies, out indicies, origin, _wallResolution * hDir, 1, 0.1f);
-                var identifier = new WallIdentifier(origin, new Vector3(origin.X + _wallResolution * hDir, origin.Y, origin.Z));
+                var origin = new Vector3(_strokeOrigin.X + _wallResolution*i*hDir, _strokeOrigin.Y, _strokeOrigin.Z);
+                MeshHelper.GenerateCube(out verticies, out indicies, origin, _wallResolution*hDir, 1, 0.1f);
+                var identifier = new WallIdentifier(origin, new Vector3(origin.X + _wallResolution*hDir, origin.Y, origin.Z));
                 _tempWallBuffer.AddObject(identifier, indicies, verticies);
                 _tempWallIdentifiers.Add(identifier);
             }
-            for (int i = 0; i < Math.Abs(strokeH); i++) {
+            for (int i = 0; i < Math.Abs(strokeH); i++){
                 int[] indicies;
                 VertexPositionNormalTexture[] verticies;
-                var origin = new Vector3(_strokeOrigin.X + _wallResolution * i * hDir, _strokeOrigin.Y, _strokeEnd.Z);
-                MeshHelper.GenerateCube(out verticies, out indicies, origin, _wallResolution * hDir, 1, 0.1f);
-                var identifier = new WallIdentifier(origin, new Vector3(origin.X + _wallResolution * hDir, origin.Y, origin.Z));
+                var origin = new Vector3(_strokeOrigin.X + _wallResolution*i*hDir, _strokeOrigin.Y, _strokeEnd.Z);
+                MeshHelper.GenerateCube(out verticies, out indicies, origin, _wallResolution*hDir, 1, 0.1f);
+                var identifier = new WallIdentifier(origin, new Vector3(origin.X + _wallResolution*hDir, origin.Y, origin.Z));
                 _tempWallBuffer.AddObject(identifier, indicies, verticies);
                 _tempWallIdentifiers.Add(identifier);
             }
@@ -308,9 +331,12 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
             _tempWallBuffer.UpdateBuffers();
         }
     }
-    struct WallIdentifier {
-        readonly public Vector3 StartPoint;
-        readonly public Vector3 EndPoint;
+
+    #region wallidentifier
+
+    internal struct WallIdentifier{
+        public readonly Vector3 EndPoint;
+        public readonly Vector3 StartPoint;
 
         public WallIdentifier(Vector3 startPoint, Vector3 endPoint){
             StartPoint = startPoint;
@@ -318,7 +344,8 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
         }
 
         #region equality operators
-        public static bool operator ==(WallIdentifier wallid1, WallIdentifier wallid2) {
+
+        public static bool operator ==(WallIdentifier wallid1, WallIdentifier wallid2){
             if (wallid1.StartPoint == wallid2.StartPoint && wallid1.EndPoint == wallid2.EndPoint)
                 return true;
             if (wallid1.StartPoint == wallid2.EndPoint && wallid2.EndPoint == wallid1.StartPoint)
@@ -326,7 +353,7 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
             return false;
         }
 
-        public static bool operator !=(WallIdentifier wallid1, WallIdentifier wallid2) {
+        public static bool operator !=(WallIdentifier wallid1, WallIdentifier wallid2){
             if (wallid1.StartPoint == wallid2.StartPoint && wallid1.EndPoint == wallid2.EndPoint)
                 return false;
             if (wallid1.StartPoint == wallid2.EndPoint && wallid2.EndPoint == wallid1.StartPoint)
@@ -335,9 +362,9 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
         }
 
         public bool Equals(WallIdentifier other){
-            if (this.StartPoint == other.StartPoint && this.EndPoint == other.EndPoint)
+            if (StartPoint == other.StartPoint && EndPoint == other.EndPoint)
                 return true;
-            if (this.StartPoint == other.EndPoint && other.EndPoint == this.StartPoint)
+            if (StartPoint == other.EndPoint && other.EndPoint == StartPoint)
                 return true;
             return false;
         }
@@ -347,14 +374,17 @@ namespace Drydock.Logic.DoodadEditorState.Tools{
             if (obj.GetType() != typeof (WallIdentifier)) return false;
             return Equals((WallIdentifier) obj);
         }
+
         #endregion
 
-        public override int GetHashCode() {
-            unchecked {
-// ReSharper disable NonReadonlyFieldInGetHashCode
-                return (StartPoint.GetHashCode() * 397) ^ EndPoint.GetHashCode();
-// ReSharper restore NonReadonlyFieldInGetHashCode
+        public override int GetHashCode(){
+            unchecked{
+                // ReSharper disable NonReadonlyFieldInGetHashCode
+                return (StartPoint.GetHashCode()*397) ^ EndPoint.GetHashCode();
+                // ReSharper restore NonReadonlyFieldInGetHashCode
             }
         }
     }
+
+    #endregion
 }
