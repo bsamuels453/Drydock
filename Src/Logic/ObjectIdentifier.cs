acd70f895fd553c509214cbaa6662ef8ccc7ecb6
Ben Samuels <bsamuels453@gmail.com>
2013-02-23 12:10:07 -0800

    implementing ObjectIdentifier class

---
+++ Src/Logic/ObjectIdentifier.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System;
+using Microsoft.Xna.Framework;
+
+#endregion
+
+namespace Drydock.Logic{
+    internal class ObjectIdentifier : IEquatable<ObjectIdentifier>{
+        public readonly int Deck;
+        public readonly ObjectType ObjectType;
+        public readonly Vector3 Position;
+
+        public ObjectIdentifier(ObjectType objectType, Vector3 position, int deck){
+            ObjectType = objectType;
+            Position = position;
+            Deck = deck;
+        }
+
+        #region IEquatable<ObjectIdentifier> Members
+
+        public bool Equals(ObjectIdentifier other){
+            return ObjectType == other.ObjectType && Position == other.Position;
+        }
+
+        #endregion
+    }
+
+    internal enum ObjectType{
+        Ladder
+    }
+}
